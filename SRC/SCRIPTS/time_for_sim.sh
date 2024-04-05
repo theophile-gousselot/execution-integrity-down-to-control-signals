9e88a59492c448c784efd5ad86a6256a3316d08e
theophile.gousselot <theophile.gousselot@emse.fr>
2024-04-05 18:18:28 +0200

    bash script to measure questa sim delay

--- SRC/SCRIPTS/time_for_sim.sh
+++ SRC/SCRIPTS/time_for_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-questa_folders=$(find $1 -iname "*questa" | grep "\.sim") 
+questa_folders=$(find $1 -iname "*questa" | grep "\.sim" | sort) 
 
 
 for folder in ${questa_folders}
@@ -8,17 +8,18 @@ do
 	if test -f "${folder}/simulate.log"; then
 		if test -f "${folder}/compile.log"; then
 			if cat ${folder}/simulate.log | grep -q "EXIT VALID EXECUTION"; then
-				simulate_end=$(stat -c '%Y' ${folder}/simulate.log)
-				compile_end=$(stat -c '%Y' ${folder}/compile.log)
-				simulation_delay=$((${simulate_end}-${compile_end}))
-				echo "${folder} ===> $(date -ud "@${simulation_delay}" +"$(( ${simulation_delay}/3600/24 ))j %H:%M:%S")"
+				REASON_END="VALID"
 			elif cat ${folder}/simulate.log | grep -q "MAXIMUM CYCLE LIMIT"; then
-				echo "${folder} ===> MAXIMUM CYCLE LIMIT"
+				REASON_END="MAXIMUM CYCLE LIMIT"
 			elif cat ${folder}/simulate.log | grep -q "EXIT ILLEGAL INSN DECODE"; then
-				echo "${folder} ===> EXIT ILLEGAL INSN DECODE"
+				REASON_END="ILLEGAL INSN"
 			else
-				echo "${folder} ===> ERROR NOT KNOW/IN PROGRESS"
+				REASON_END="ERROR NOT KNOW/IN PROGRESS"
 			fi
+			simulate_end=$(stat -c '%Y' ${folder}/simulate.log)
+			compile_end=$(stat -c '%Y' ${folder}/compile.log)
+			simulation_delay=$((${simulate_end}-${compile_end}))
+			echo "$(date -ud "@${simulation_delay}" +"$(( ${simulation_delay}/3600/24 ))j %H:%M:%S") --- ${folder} ===>  ${REASON_END}"
 		fi
 	fi
 done
